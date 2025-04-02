2e4c583e038ac6f7c9adb47a9f894c0e5a9e2b97
creamp_ <pattarawan654@gmail.com>
2025-04-02 15:52:33 +0700

    .

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
     host: 'trolley.proxy.rlwy.net', 
-    port: 3306,
+    port: 23700,
     user: 'root', 
     password: 'hQvrcrVRjUXlJImHRJqXJRpcJygReUqR', 
     database: 'railway', 
